2eea65ac71e8d71cafea48f12358376dd25d3e73
Rafael Fernandes de Oliveira <rafaelolivei@gmail.com>
2014-02-23 17:56:36 -0300

    Concluído o exercício de fragments
    A aplicação funciona, mas o teste não. Tem que ver.

--- FragmentsLab/src/course/labs/fragmentslab/MainActivity.java
+++ FragmentsLab/src/course/labs/fragmentslab/MainActivity.java
@@ -27,10 +27,13 @@ public class MainActivity extends Activity implements
 			mFriendsFragment = new FriendsFragment();
 
 			//TODO 1 - add the FriendsFragment to the fragment_container
+//			FragmentTransaction ft = getFragmentManager().beginTransaction();
+//			ft.add(R.id.friends_frag,  mFriendsFragment);
+//			ft.commit();
 			FragmentTransaction ft = getFragmentManager().beginTransaction();
-			ft.add(R.id.friends_frag, mFriendsFragment);
+			ft.add(R.id.friends_frag,  mFriendsFragment);
 			ft.commit();
-
+			
 		} else {
 
 			// Otherwise, save a reference to the FeedFragment for later use
@@ -66,13 +69,10 @@ public class MainActivity extends Activity implements
 		if (!isInTwoPaneMode()) {
 
 			//TODO 2 - replace the fragment_container with the FeedFragment
-			mFeedFragment = new FeedFragment();
 			FragmentTransaction trans = getFragmentManager().beginTransaction();
 			trans.replace(R.id.feed_frag, mFeedFragment);
 			trans.addToBackStack(null);
-			trans.commit();
-
-			
+			trans.commit();			
 
 			// execute transaction now
 			getFragmentManager().executePendingTransactions();
