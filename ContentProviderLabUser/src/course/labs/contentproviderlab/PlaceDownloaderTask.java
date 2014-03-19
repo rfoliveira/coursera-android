3e77f48fbb80d5624dc76715bda9f0637a1894e4
Rafael Oliveira <rafaelolivei@gmail.com>
2014-03-19 14:25:15 -0300

    Concluídos os TODO's da semana 8
    Porém não passa nos testes.

--- ContentProviderLabUser/src/course/labs/contentproviderlab/PlaceDownloaderTask.java
+++ ContentProviderLabUser/src/course/labs/contentproviderlab/PlaceDownloaderTask.java
@@ -1,4 +1,4 @@
-package course.labs.locationlab;
+package course.labs.contentproviderlab;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -31,30 +31,30 @@ import android.util.Log;
 public class PlaceDownloaderTask extends AsyncTask<Location, Void, PlaceRecord> {
 
 	// Change to false if you don't have network access
-	private static final boolean HAS_NETWORK = false;
+	private static final boolean HAS_NETWORK = true;
 
-    // TODO - put your www.geonames.org account name here.
-    private static String USERNAME = "rfoliveira";
+	// TODO - put your www.geonames.org account name here.
+	private static String USERNAME = "rfoliveira";
 
 	private HttpURLConnection mHttpUrl;
 	private WeakReference<PlaceViewActivity> mParent;
 	private static Bitmap mStubBitmap;
-	private static Location mockLoc1 = new Location(LocationManager.NETWORK_PROVIDER);
-	private static Location mockLoc2 = new Location(LocationManager.NETWORK_PROVIDER);
-
+	private static Location mockLoc1 = new Location(
+			LocationManager.NETWORK_PROVIDER);
+	private static Location mockLoc2 = new Location(
+			LocationManager.NETWORK_PROVIDER);
 
 	public PlaceDownloaderTask(PlaceViewActivity parent) {
 		super();
 		mParent = new WeakReference<PlaceViewActivity>(parent);
 
 		if (!HAS_NETWORK) {
-			mStubBitmap = BitmapFactory.decodeResource(parent.getResources(),R.drawable.stub);
-
+			mStubBitmap = BitmapFactory.decodeResource(parent.getResources(),
+					R.drawable.stub);
 			mockLoc1.setLatitude(37.422);
 			mockLoc1.setLongitude(-122.084);
-			
 			mockLoc2.setLatitude(38.996667);
-			mockLoc2.setLongitude(-76.9275);			
+			mockLoc2.setLongitude(-76.9275);
 		}
 	}
 
@@ -73,17 +73,18 @@ public class PlaceDownloaderTask extends AsyncTask<Location, Void, PlaceRecord> 
 			} else {
 				place = null;
 			}
-
 		} else {
 			place = new PlaceRecord(location[0]);
 			if (location[0].distanceTo(mockLoc1) < 100) {
 				place.setCountryName("United States");
 				place.setPlace("The Greenhouse");
 				place.setFlagBitmap(mStubBitmap);
+                place.setFlagUrl("stub.jpg");
 			} else {
 				place.setCountryName("United States");
 				place.setPlace("Berwyn");
 				place.setFlagBitmap(mStubBitmap);
+                place.setFlagUrl("stub.jpg");
 			}
 		}
 
@@ -104,7 +105,6 @@ public class PlaceDownloaderTask extends AsyncTask<Location, Void, PlaceRecord> 
 		BufferedReader in = null;
 
 		try {
-
 			URL url = new URL(params[0]);
 			mHttpUrl = (HttpURLConnection) url.openConnection();
 			in = new BufferedReader(new InputStreamReader(
@@ -138,8 +138,6 @@ public class PlaceDownloaderTask extends AsyncTask<Location, Void, PlaceRecord> 
 	private Bitmap getFlagFromURL(String flagUrl) {
 
 		InputStream in = null;
-
-		Log.i("temp", flagUrl);
 
 		try {
 			URL url = new URL(flagUrl);
@@ -196,7 +194,7 @@ public class PlaceDownloaderTask extends AsyncTask<Location, Void, PlaceRecord> 
 							countryCode = curr2.getTextContent();
 						} else if (curr2.getNodeName().equals("name")) {
 							placeName = curr2.getTextContent();
-						} 
+						}
 					}
 				}
 			}
@@ -211,7 +209,7 @@ public class PlaceDownloaderTask extends AsyncTask<Location, Void, PlaceRecord> 
 		}
 
 		return new PlaceRecord(generateFlagURL(countryCode.toLowerCase()),
-				countryName, placeName);
+				null, countryName, placeName, -1, -1);
 	}
 
 	private static String generateURL(String username, Location location) {
